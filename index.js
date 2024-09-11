138cc54e25e46d90adab6f3415e39cbf8b2175e4
unknown <adesiyantope2014@gmail.com>
2024-09-11 16:38:48 +0100

    endpoint

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/v1/get-eligible-triggers", async (req, res) => {
     console.error("Error forwarding request:", error);
     res.status(500).send("An error occurred while processing your request.");
   }
-});
+})
 
 app.get("/", async (req, res) => {
   console.log(req);
