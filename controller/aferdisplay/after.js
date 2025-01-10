d9b08ba59451d42e82b4cefc2df1e47c7ebd3676
TemitopeAde <adesiyantope2014@gmail.com>
2025-01-10 09:30:25 +0100

    webhook

--- controller/aferdisplay/after.js
+++ controller/aferdisplay/after.js
@@ -3,16 +3,16 @@ const { appInstances } = require("@wix/app-management");
 
 
 const PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApBescsmw4DUGR0Tz5oKA
-C5coDQeypqYyBbjOToJsirk7OHhTV0p/0rcwhTQyYtZvfljGVryHKGuuDAL2h3hf
-ULDXPGI4uW/l1rUzHlrmqyoAtdZR9oAYAabFo3Kutzq7QG41NmaSRX6tlubOqgXF
-pRq1Rlxs/GvKqnd6VHDQWWuEweIVaawv3xPG3MDCMmFOyZSs0r+HOPq5ZkDkIvqv
-SzStiisd6dhwUcHMSpcVNn0wwCw7dNyDg1q4aX8mFze16NC0jhVSm3e09cGHb4oW
-z2JOoed5UPGH8TF8SzQGLwMevgAQ7pD2BZcPfT24vF6/PcWRtQKb4JxzPTUs0zMG
-dwIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzlMj3TB1OJWLhLBDNT9W
+XpNsHPKNiZZb4oH6NcG/ec/DRQeoH3Bc5Sh72BSQxCtnkKUNqoVaQ58+ckYoC3Sl
+POm2kV2lIDL2mnyiLN8QmXriMvOn0SHY1GpidnCzkMjcB4aK+JujA3TlalpumEjG
+NGhiezV1Tc7EOYLgFDYQV+XIvFwSaU/tLfiqTX8T94RTUCFPt/8S1grzyt8itESH
+KDBIfESetvxbouPDOK2zfhXtCVXgNZ/stMajVkca30xE5jCijM7CkY99XuyK3Dhn
+3n4QRDaJDtmGJw6PWoINPJmI9JyCcI79uVWQypLgQRMioNLP/glxxGUardveWyio
+sQIDAQAB
 -----END PUBLIC KEY-----`;
 
-const APP_ID = "4a388e84-5edb-4412-a263-0275d898ed9a";
+const APP_ID = "0e9a195f-e6cd-4b6d-9eda-3a221105e46c";
 
 const client = createClient({
   auth: AppStrategy({
