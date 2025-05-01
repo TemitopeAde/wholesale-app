83cb7954c85805a8815cd2d2f2df95d399fb74bf
TemitopeAde <adesiyantope2014@gmail.com>
2025-05-01 10:24:53 +0100

    emails

--- utils/app.js
+++ utils/app.js
@@ -1,21 +1,15 @@
-// utils/pdf.js
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const generatePdfBuffer = async (htmlContent) => {
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+    headless: 'new', // Or true
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
-
   const pdfBuffer = await page.pdf({ format: 'A4' });
 
   await browser.close();
   return pdfBuffer;
 };
-
-module.exports = generatePdfBuffer;
